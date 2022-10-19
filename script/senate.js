282cbc191642f83fb793714aaa57d811913109e4
herrerogusano <herrero.gutierrez.alberto@gmail.com>
2022-10-19 18:14:48 +0200

    filter by party done!

--- script/senate.js
+++ script/senate.js
@@ -16,12 +16,12 @@ import {data} from './pro-congress-117-senate.mjs';
   });
  } */
 
- const members = data.results[0].members;
- createTable(members);
+//TABLES
 
+ const members = data.results[0].members;
+ 
  const createTable = (array) => {
-  let tbody = document.getElementById("senateData");
-  tbody.innerHTML = "";
+  document.getElementById("tbody").innerHTML = "";
   for(let i=0;i<array.length;i++) {
     //create new rows for each element of the list
     const newRow = document.createElement("tr");
@@ -31,22 +31,74 @@ import {data} from './pro-congress-117-senate.mjs';
       middleName = "";
     }  
     
-    let tdName = document.createElement("td");
+    let anchor = document.createElement("a"); //create anchor tag
+    anchor.setAttribute('href', array[i].url); //direction to find url
+    anchor.textContent = array[i].first_name + " " +  middleName + " " + array[i].last_name; //content anchor
+    let tdName = document.createElement("td"); //create td's of the table
     let tdParty = document.createElement("td");
     let tdState = document.createElement("td");
     let tdYearsOffice = document.createElement("td");
     let tdVotes = document.createElement("td");
 
-    tdName.innerHTML= array[i].first_name + " " +  middleName + " " + array[i].last_name;
+    tdName.appendChild(anchor); //add the anchor to the td tag
+    /* tdName.innerHTML= array[i].first_name + " " +  middleName + " " + array[i].last_name; */
     tdParty.innerHTML= array[i].party;
     tdState.innerHTML= array[i].state;
     tdYearsOffice.innerHTML= array[i].seniority;
     tdVotes.innerHTML= array[i].votes_with_party_pct;
-    newRow.append(tdName, tdParty, tdState, tdYearsOffice, tdVotes);
-    tbody.append(newRow); 
-    
+    newRow.append(tdName, tdParty, tdState, tdYearsOffice, tdVotes); // append all td's to the same row
+    tbody.append(newRow); // append every new row to the tbody (table)
   }
  }
-
+ 
+ createTable(members);
+ let checked_nodeList = document.querySelectorAll("input[type=checkbox]");
+ let check = [];
+ 
+ const parties = {
+  R: "Republicans",
+  D: "Democrats",
+  ID: "Independents"
+ }
 
  
+//FILTER TABLES
+
+const checkedbox = (array) => {
+  check = Array.from(checked_nodeList) //converts checked_array that is a node list to an array
+            .filter(i=>i.checked) //filter the checked ones
+            .map(i=>i.value); //create a new array with the values of the checked ones
+  console.log(check, 'check') 
+  let filtered_array = []; //array we are gonna fill with filtered members
+  array.forEach(element => { //we use forEach method to check every element of the array members with the condition below
+    if (check.includes(element.party)) {
+      filtered_array.push(element);
+  }
+})
+return createTable(filtered_array);
+}
+
+
+//SELECTED CHECKBOX
+let enabledSettings = []
+const allEventListener = (array) => {
+// Use Array.forEach to add an event listener to each checkbox.
+checked_nodeList.forEach(function(checkbox) {
+  checkbox.addEventListener('change', function() {
+    enabledSettings = 
+      Array.from(checked_nodeList) // Convert checkboxes to an array to use filter and map.
+      .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+      .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
+      console.log(enabledSettings,)
+    
+    checkedbox(members);
+  })
+});
+}
+allEventListener(members);
+
+
+
+
+
+
